cdcc5b9e9af161c0dfa3fe99a6f4b5346b6718dd
DivyanshPradhan <divyansh00pradhan@gmail.com>
2018-04-29 10:51:02 +0530

    Initial

--- app/src/main/java/com/example/sweet/powertymitigation/Home.java
+++ app/src/main/java/com/example/sweet/powertymitigation/Home.java
@@ -9,34 +9,9 @@ import java.util.HashMap;
 
 public class Home extends AppCompatActivity {
 
-   /* ListView listView;
-    CustomAdapter customAdapter;
-    HashMap<ArrayList<String>, ArrayList<String>> data;
-    public static ArrayList<String> subtitle;
-    public static ArrayList<String> title;
-*/
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
     }
 }
-   /*     listView = findViewById(R.id.listView);
-        populateData();
-        customAdapter = new CustomAdapter(Home.this);
-        listView.setAdapter(customAdapter);
-    }*/
-
-   /* public void populateData()
-    {
-        title = new ArrayList<>();
-        title.add("title1");
-        title.add("title2");
-        title.add("title3");
-
-        subtitle = new ArrayList<>();
-        subtitle.add("subtitle1");
-        subtitle.add("subtitle2");
-        subtitle.add("subtitle3");
-    }
-}*/
